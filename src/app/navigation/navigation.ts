09b259d66dfe00a3c4f025e3c5099b27e83480b4
Ruben Cancino <luxsoft.cancino@gmail.com>
2019-03-08 16:12:04 -0600

    pruebas

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -1,69 +1,110 @@
 import { FuseNavigation, FuseNavigationItem } from '@fuse/types';
 
 const polizasIngreso: FuseNavigationItem[] = [
-    {
-        id: 'ingreso',
-        title: 'Ingreso',
-        type: 'collapsable',
-        icon: 'call_received',
-        children: []
-    }
+  {
+    id: 'ingreso',
+    title: 'Ingreso',
+    type: 'collapsable',
+    icon: 'call_received',
+    children: [
+      {
+        id: 'cobranzaCredito',
+        title: 'Cobranza CRE',
+        type: 'item',
+        icon: 'receipt',
+        url: 'polizas/cobranza/cre'
+      }
+    ]
+  }
 ];
 
 const polizasEgresos: FuseNavigationItem[] = [
-    {
-        id: 'egreso',
-        title: 'Egreso',
-        type: 'collapsable',
-        icon: 'call_made',
-        children: []
-    }
+  {
+    id: 'egreso',
+    title: 'Egreso',
+    type: 'collapsable',
+    icon: 'call_made',
+    children: []
+  }
 ];
 
 const polizasDiario: FuseNavigationItem[] = [
-    {
-        id: 'ingreso',
-        title: 'Ingreso',
-        type: 'collapsable',
-        icon: 'view_stream',
-        children: []
-    }
+  {
+    id: 'ingreso',
+    title: 'Ingreso',
+    type: 'collapsable',
+    icon: 'view_stream',
+    children: []
+  }
 ];
 
 const polizasNavigation: FuseNavigationItem[] = [
-    {
-        id: 'polizas',
-        title: 'Polizas',
-        icon: 'receipt',
-        type: 'collapsable',
-        children: [...polizasIngreso, ...polizasEgresos, ...polizasDiario]
-    }
+  {
+    id: 'polizas',
+    title: 'Polizas',
+    icon: 'receipt',
+    type: 'collapsable',
+    children: [...polizasIngreso, ...polizasEgresos, ...polizasDiario]
+  }
 ];
 
 export const navigation: FuseNavigation[] = [
-    {
-        id: 'contabilidad',
-        title: 'Contabilidad',
-        type: 'group',
-        icon: 'apps',
+  {
+    id: 'contabilidad',
+    title: 'Contabilidad',
+    type: 'group',
+    icon: 'apps',
+    children: [
+      {
+        id: 'catalogos',
+        title: 'Catalogos',
+        type: 'collapsable',
+        icon: 'web',
         children: [
-            {
-                id: 'catalogos',
-                title: 'Catalogos',
-                type: 'collapsable',
-                icon: 'web',
-                children: [
-                    {
-                        id: 'cuentas',
-                        title: 'Cuentas contables',
-                        type: 'item',
-                        url: '/cuentas',
-                        icon: 'storage',
-                        exactMatch: true
-                    }
-                ]
-            },
-            ...polizasNavigation
+          {
+            id: 'cuentas',
+            title: 'Cuentas contables',
+            type: 'item',
+            url: '/cuentas',
+            icon: 'storage',
+            exactMatch: true
+          }
         ]
-    }
+      },
+      ...polizasNavigation
+    ]
+  },
+  {
+    id: 'sat',
+    title: 'SAT',
+    type: 'group'
+  },
+  {
+    id: 'activo-fijo',
+    title: 'Activo Fijo',
+    type: 'group',
+    icon: 'apps',
+    children: []
+  },
+  {
+    id: 'analytics',
+    title: 'Analytics',
+    type: 'group',
+    children: [
+      {
+        id: 'dashboards',
+        title: 'Tableros',
+        type: 'collapsable',
+        icon: 'dashboard',
+        children: []
+      },
+      {
+        id: 'reportes',
+        title: 'Reportes',
+        type: 'collapsable',
+        icon: 'dashboard',
+        children: []
+      }
+    ]
+  }
 ];
